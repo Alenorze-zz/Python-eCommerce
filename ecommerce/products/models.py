414b414d27fbef88122a7ed3cb027d8b4ddb189e
Алексей Белов <Alenorze@gmail.com>
2017-11-30 00:10:42 +0200

    Django Admin

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title           = models.Charfield(max_lenght=120)
+    title           = models.CharField(max_lenght=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title

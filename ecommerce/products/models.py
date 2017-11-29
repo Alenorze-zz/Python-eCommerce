3bca36eeffbdf62bdaadf9c9a07b55de7e4f2498
Алексей Белов <Alenorze@gmail.com>
2017-11-30 01:02:54 +0200

    Image Field

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
-# Create your models here.
+import random
+import os
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1,3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
+
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)
-
+    image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title

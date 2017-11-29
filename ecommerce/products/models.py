fb86e6354ba4d26c7883ffcc0813a75fc13296f0
Алексей Белов <Alenorze@gmail.com>
2017-11-30 01:43:49 +0200

    Product Manager

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -16,12 +16,22 @@ def upload_image_path(instance, filename):
     return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_query().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
+    objects = ProductManager()
+    
     def __str__(self):
         return self.title
 
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.http import Http404
 
 from .models import Product
+
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        raise instance

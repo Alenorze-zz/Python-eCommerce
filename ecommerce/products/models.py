dbeafd6e74b842091a04dbfd36c7777a1c1085ed
Алексей Белов <Alenorze@gmail.com>
2017-11-30 00:04:19 +0200

    Product Model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title           = models.Charfield(max_lenght=120)
+    description     = models.TextField()
+    price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)

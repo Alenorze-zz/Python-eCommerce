d53a2c3a0fa9dfb0b01ea8a2585d9e49212e948a
Алексей Белов <Alenorze@gmail.com>
2017-11-30 23:34:56 +0200

    Reverse for URLs

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
-    url(r'^products/', include("products.urls")),
+    url(r'^products/', include("products.urls", namespace='products')),
     url(r'^admin/', admin.site.urls),
 ]
 
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 from .utils import unique_slug_generator
 
@@ -56,7 +57,7 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug}))
     
     def __str__(self):
         return self.title

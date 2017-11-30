32382f77638a17992432e59fec5a13aa41302391
Алексей Белов <Alenorze@gmail.com>
2017-11-30 21:36:51 +0200

    Featured View

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -16,6 +16,14 @@ def upload_image_path(instance, filename):
     return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+    
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+
 class ProductManager(models.Manager):
     def get_by_id(self, id):
         qs = self.get_query().filter(id=id)
@@ -23,12 +31,23 @@ class ProductManager(models.Manager):
             return qs.first()
         return None
 
+    def get_queryset(self):
+        return ProductQuerySet(models.query.QuerySet)
+
+    def all(self):
+        return self.get_queryset().active()
+
+     def featured(self):
+        return self.get_queryset().featured()
+
 
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    active          = models.BooleanField(default=True)
+    featured        = models.BooleanField(default=False)
 
     objects = ProductManager()
     
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -5,6 +5,19 @@ from django.http import Http404
 from .models import Product
 
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
@@ -28,3 +41,5 @@ class ProductDetailView(DetailView):
         if instance is None:
             raise Http404("Product doesn't exist")
         raise instance
+
+

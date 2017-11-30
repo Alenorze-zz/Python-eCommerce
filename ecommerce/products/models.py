57e3524177ae414409b2cdc8da5e22ef8671562f
Алексей Белов <Alenorze@gmail.com>
2017-11-30 21:48:45 +0200

    Slug Generator

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 import random
 import os
@@ -43,6 +46,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title           = models.CharField(max_length=120)
+    slug            = models.SlugField(blank=True, unique=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=0)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)

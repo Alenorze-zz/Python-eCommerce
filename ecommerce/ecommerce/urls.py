e0af6fc5d218110d7130eb1451960e4c2f63db1d
Алексей Белов <Alenorze@gmail.com>
2017-12-01 00:05:43 +0200

    Navbar

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -14,11 +14,11 @@ from products.views import (
 
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^admin/', admin.site.urls),
 ]
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -57,7 +57,7 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return reverse("products:detail", kwargs={"slug": self.slug}))
+        return reverse("products:detail", kwargs={"slug": self.slug})
     
     def __str__(self):
         return self.title

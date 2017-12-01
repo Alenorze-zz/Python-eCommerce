bb32e4dc945963ac3fdcf93671018cc6dd6357be
Алексей Белов <Alenorze@gmail.com>
2017-12-01 16:53:22 +0200

    Cart View

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
+from .models import Cart
+
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    return cart_obj
+
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None)
     if cart_id is None:
-        request.session['cart_id'] = 12
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
+    else:
+        qs = Cart.objects.filter(id=cart_id)
+        if qs.count() == 1:
+            cart_obj = qs.first()
+        else:
+            cart_obj = cart_create()
+            request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})

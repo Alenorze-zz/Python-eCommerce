2b4ee6483a5a4cc8216b0f2c5583f43ab9ba9e3c
Алексей Белов <Alenorze@gmail.com>
2017-12-01 17:08:12 +0200

    Cart Manager

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -10,14 +10,13 @@ def cart_create(user=None):
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None)
-    if cart_id is None:
-        cart_obj = cart_create()
-        request.session['cart_id'] = cart_obj.id
+    qs = Cart.objects.filter(id=card_id)
+    if qs.count() == 1:
+        cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            cart_obj = qs.first()
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})

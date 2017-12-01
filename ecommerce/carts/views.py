1cfb9b24f4ac43ef0d2c73edfcc6ac5edd04dc22
Алексей Белов <Alenorze@gmail.com>
2017-12-01 21:52:33 +0200

    Cart Update View

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from products.models import Product
 from .models import Cart
 
 
@@ -17,3 +18,15 @@ def cart_home(request):
     cart_obj.total = total
     cart_obj.save()
     return render(request, "carts/home.html", {})
+
+
+def cart_update(request):
+    product_id = 1
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
+    
+    return redirect("cart:home")
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import Http404
 
+from carts.models import Cart
 from .models import Product
 
 
@@ -30,6 +31,12 @@ class ProductListView(ListView):
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
 
     def get_object(self, *args, **kwargs):
         request = self.request

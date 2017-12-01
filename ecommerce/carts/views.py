ad545666b07e6996d68447a8062d5d2475765b5a
Алексей Белов <Alenorze@gmail.com>
2017-12-01 22:17:34 +0200

    Cart Form

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -21,12 +21,16 @@ def cart_home(request):
 
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    product_id = request.POST.get(id='product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExists:
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
     
     return redirect("cart:home")

24f4bcea7fdc7e03593867fb696091df34506748
Алексей Белов <Alenorze@gmail.com>
2017-12-02 14:12:55 +0200

    Accounts Form

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -30,11 +30,6 @@ def contact_page(request):
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
-    # if request.method == "POST":
-    #     #print(request.POST)
-    #     print(request.POST.get('fullname'))
-    #     print(request.POST.get('email'))
-    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
 
@@ -43,25 +38,14 @@ def login_page(request):
     context = {
         "form": form
     }
-    print("User logged in")
-    #print(request.user.is_authenticated())
     if form.is_valid():
         print(form.cleaned_data)
         username  = form.cleaned_data.get("username")
         password  = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
-        print(user)
-        #print(request.user.is_authenticated())
         if user is not None:
-            #print(request.user.is_authenticated())
             login(request, user)
-            # Redirect to a success page.
-            #context['form'] = LoginForm()
             return redirect("/")
-        else:
-            # Return an 'invalid login' error message.
-            print("Error")
-
     return render(request, "auth/login.html", context)
 
 
@@ -72,13 +56,10 @@ def register_page(request):
         "form": form
     }
     if form.is_valid():
-        print(form.cleaned_data)
         username  = form.cleaned_data.get("username")
         email  = form.cleaned_data.get("email")
         password  = form.cleaned_data.get("password")
         new_user  = User.objects.create_user(username, email, password)
-        print(new_user)
-
     return render(request, "auth/register.html", context)
 
 

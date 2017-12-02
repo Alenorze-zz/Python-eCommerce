2e26b464128ffb915c35fb5bec6600bd5fa668e7
Алексей Белов <Alenorze@gmail.com>
2017-12-02 13:40:31 +0200

    Checkout View

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -28,3 +28,13 @@ def cart_update(request):
             cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
     return redirect("cart:home")
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0: 
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object": order_obj})

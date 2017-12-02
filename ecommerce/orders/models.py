edf297d5502504e89dae183260f6ee2bfd3eadc0
Алексей Белов <Alenorze@gmail.com>
2017-12-02 13:17:18 +0200

    Generate the Order ID

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from dajngo.db.models.signals import pre_save
 
 from carts.models import Cart
+from ecommerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -19,3 +21,9 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
--- ecommerce/tags/models.py
+++ ecommerce/tags/models.py
@@ -3,7 +3,7 @@ from django.db.models.signals import pre_save, post_save
 from django.core.urlresolvers import reverse
 
 from products.models import Product
-from products.utils import unique_slug_generator
+from ecommerce.utils import unique_slug_generator
 
 
 class Tag(models.Model):

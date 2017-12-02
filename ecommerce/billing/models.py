1848e3e06a7b15d294be34371faff3b7ff097ae6
Алексей Белов <Alenorze@gmail.com>
2017-12-02 15:04:35 +0200

    Billing Model

--- ecommerce/billing/models.py
+++ ecommerce/billing/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class BillingProfile(models.Model):
+    user      = models.OneToOneField(User, null=True, blank=True)
+    email     = models.EmailField()
+    active    = models.BooleanField(default=True)
+    update    = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
+
+post_save.connect(user_created_receiver, sender=User)
+
+    
+

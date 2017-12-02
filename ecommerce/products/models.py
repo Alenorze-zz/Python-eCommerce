5c9df00db9de87edd76c593c8ba2737aaa2bd909
Алексей Белов <Alenorze@gmail.com>
2017-12-02 02:37:47 +0200

    Display Cart

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -78,6 +78,10 @@ class Product(models.Model):
     def __unicode__(self):
         return self.title
 
+    @property
+    def name(self):
+        return self.title
+        
 
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:

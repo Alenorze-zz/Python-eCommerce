fb387ae2e9db17068b523c64f815d3a327084654
Алексей Белов <Alenorze@gmail.com>
2017-12-03 20:37:59 +0200

    Handle Ajax in Django with JsonResponse

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from addresses.forms import AddressForm
 from addresses.models import Address
@@ -26,9 +27,17 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
-            cart_obj.products.add(product_obj) 
+            cart_obj.products.add(product_obj)
+            added = True 
         request.session['cart_items'] = cart_obj.products.count()
+        if request.is_ajax():
+            json_data = {
+                "added": added,
+                "removed": not added,
+            }
+            return JsonResponse(json_data)
     return redirect("cart:home")
 
 

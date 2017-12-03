4cc4da931ff41153367f85c621ee44bb41655fbc
Алексей Белов <Alenorze@gmail.com>
2017-12-03 16:36:27 +0200

    Checkout Address View

--- ecommerce/addresses/admin.py
+++ ecommerce/addresses/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address
+
+admin.site.register(Address)
--- ecommerce/addresses/models.py
+++ ecommerce/addresses/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from billing.models import BillingProfile
+
+ADDRESS_TYPES = (
+    ('billing', 'Billing'),
+    ('shipping', 'Shipping')
+)
+
+
+class Address(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile)
+    address_type    = models.CharField(max_length=120, choices=ADDRESS_TYPES)
+    address_line    = models.CharField(max_length=120)
+    address_line    = models.CharField(max_length=120, null=True, blank=True)
+    city            = models.CharField(max_length=120)
+    country         = models.CharField(max_length=120, default='Ukraine')
+    state           = models.CharField(max_length=120)
+    postal_code     = models.CharField(max_length=120)
+
+    def __srt__(self):
+        return str(self.billing_profile)
+    

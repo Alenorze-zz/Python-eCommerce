5b8c651d734882212f9359c30a93815622e74995
Алексей Белов <Alenorze@gmail.com>
2017-12-03 16:14:26 +0200

    Addresses Component

---
+++ ecommerce/addresses/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class addressesConfig(AppConfig):
+    name = 'addresses'
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'search',
     'tags',
     'accounts',
+    'addresses',
 ]
 
 MIDDLEWARE = [

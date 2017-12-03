6d49a11eea6c2decbb8a4aef57f01b0c3d9dc958
Алексей Белов <Alenorze@gmail.com>
2017-12-03 03:11:53 +0200

    Billing Profile in the Checkout View

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
+from orders.models import Order
 from products.models import Product
 from .models import Cart
 
@@ -37,4 +40,14 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request, "carts/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+    if user.is_authenticated():
+        billing_profile, billing_profile_created = BillingProfile.object.get_or_create(user=user, email=user.email)
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form
+    }    
+    return render(request, "carts/checkout.html", context)

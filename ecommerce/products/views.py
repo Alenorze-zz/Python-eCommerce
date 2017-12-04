8baf48497035a543d066c62cb98b469800437d37
Алексей Белов <Alenorze@gmail.com>
2017-12-04 20:53:21 +0200

    Analytics Signal

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import Http404
 
+from analytics.signals import object_viewed_signal
 from carts.models import Cart
 from .models import Product
 
@@ -56,6 +57,7 @@ class ProductDetailSlugView(DetailView):
             instance = qs.first()
         except:
             raise Http404("Uhhmmm ")
+        # object_viewed_signal.send(instance.__class__, instance=instance, request=request)
         return instance
 
 

cddd95309a9ea058fe2a30171cfadb750927ae9a
Алексей Белов <Alenorze@gmail.com>
2017-12-04 19:54:45 +0200

    Custom Form and Admin

--- ecommerce/accounts/admin.py
+++ ecommerce/accounts/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
 
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 from .models import GuestEmail
 
-admin.site.register(GuestEmail)
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+    list_display = ('email', 'admin')
+    list_filter = ('admin', 'staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin', 'staff', 'active',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+
+
+admin.site.unregister(Group)
+
+
+class GuestEmailAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model = GuestEmail
+
+
+admin.site.register(GuestEmail, GuestEmailAdmin)

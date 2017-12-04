b310453f7fcfb1ab4b52b416b0bd22f2fb0fd410
Алексей Белов <Alenorze@gmail.com>
2017-12-04 18:14:48 +0200

    Ajaxify the Contact Form

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render,redirect
 
 from .forms import ContactForm
@@ -31,6 +31,12 @@ def contact_page(request):
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submisssion"})
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
     return render(request, "contact/view.html", context)
 
 

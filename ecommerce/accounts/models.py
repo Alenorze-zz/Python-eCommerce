d347c8ac54b67bb27695580cb80d23d245592d6b
Алексей Белов <Alenorze@gmail.com>
2017-12-04 19:12:14 +0200

    User Manager

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,8 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import(
-    AbstractBaseUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager
 )
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("No email")
+        if not password:
+            raise ValueError("No password")
+        
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self.db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
 
 class User(AbstractBaseUser):
     email     = models.EmailField(max_length=255, unique=True)
@@ -13,6 +48,8 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email

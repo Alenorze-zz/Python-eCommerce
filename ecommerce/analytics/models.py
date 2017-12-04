95d26c3c54443b08e95a2526048ebe499cbd4d93
Алексей Белов <Alenorze@gmail.com>
2017-12-04 20:44:36 +0200

    Object Viewed Model

--- ecommerce/analytics/models.py
+++ ecommerce/analytics/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class ObjectViewed(models.Model):
+    user           = models.ForeignKey(User, blank=True, null=True)
+    ip_address     = models.CharField(max_length=120, blank=True, null=True)
+    content_type   = models.ForeignKey(ContentType)
+    object_id      = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp      = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __srt__(self):
+        return "%s viewed on %s" %(self.content_object, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = 'Object viewed'
+        verbose_name_plutal = 'Objects viewed'

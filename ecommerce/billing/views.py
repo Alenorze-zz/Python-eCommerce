90425e5540934bcf5e1c267942765be10422a832
Алексей Белов <Alenorze@gmail.com>
2017-12-06 20:02:18 +0200

    Fix

--- ecommerce/billing/views.py
+++ ecommerce/billing/views.py
@@ -1,13 +1,16 @@
 import stripe
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.utils.http import is_safe_url
 
-
-stripe.api_key = "sk_test_hlp4fgMTihQnCQkWp5bj2Suw"
+stripe.api_key = 'sk_test_hlp4fgMTihQnCQkWp5bj2Suw'
 
 STRIPE_PUB_KEY = 'pk_test_D8FSbsJiS8Lfk8sVGdyxOJf1'
 
 
 def payment_method_view(request):
-    if request.method == "POST":
-        print(request.POST)
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
     return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})

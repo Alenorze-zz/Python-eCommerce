bb7531055280bd895e9fd1fe8764b7060382d777
Алексей Белов <Alenorze@gmail.com>
2017-12-06 23:55:23 +0200

    Mailchimp Validate

--- ecommerce/marketing/utils.py
+++ ecommerce/marketing/utils.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from django.conf import settings
 
@@ -13,16 +14,29 @@ class Mailchipt(object):
         self.key = MAILCHIMP_API_KEY
         self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(dc=MAILCHIMP_DATA_CENTER)
         self.list_id = MAILCHIMP_EMAIL_LIST_ID
+        self.list_endpoint = '{api_url}/lists/{list_id}'.format(
+                                    api_url = self.api_url,
+                                    list_id = self.list_id
+                                )
     
     def check_subcription_status(self, email):
         endpoint = self.api_url
         r = requests.get(endpoint, auth=("", self.key))
         return r.json()
 
+    def check_valid_status(self, status):
+        choices =  ['subscribed', 'unsubscribed', 'cleaned', 'pending']
+        if status not in choices:
+            raise ValueError("Not valid choice")
+        return status
+
     def add_email(self, email):
+        status = "subscribed"
+        self.check_valid_status(status)
         data = {
-            "email": email
+            "email_address": email,
+            "status": status
         }
-        endpoint = self.api_url
-        r = requests.post(endpoint, auth=("", self.key), data=data)
+        endpoint = self.list_endpoint + "/members"
+        r = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
         return r.json()

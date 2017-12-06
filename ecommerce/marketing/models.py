2532c59ec4d25587f8fd597e6b5df9df8b6b9479
Алексей Белов <Alenorze@gmail.com>
2017-12-06 23:15:17 +0200

    Marketing Model

--- ecommerce/marketing/models.py
+++ ecommerce/marketing/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class MarketingPreference(models.Model):
+    user          = models.OneToOneField(settings.AUTH_USER_MODEL)
+    subscribed    = models.BooleanField(default=True)
+    mailchimp_msg = models.TextField(null=True, blank=True)
+    timestamp     = models.DateTimeField(auto_now_add=True)
+    update        = models.DateTimeField(auto_now=True)
+
+    def __srt__(self):
+        return self.user.email 
+
+
+def marketing_pref_update_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        pass 
+
+post_save.connect(marketing_pref_update_receiver, sender=MarketingPreference)
+
+
+def make_marketing_pref_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        MarketingPreference.objects.get_or_create(user=instance) 
+
+post_save.connect(make_marketing_pref_receiver, sender=settings.AUTH_USER_MODEL)

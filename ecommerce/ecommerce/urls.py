27fbf1b47d1aab376c4f9096b23a95c963301861
Алексей Белов <Alenorze@gmail.com>
2017-12-07 23:37:32 +0200

    Marketing Update View

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -8,6 +8,7 @@ from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from billing.views import payment_method_view
 from carts.views import cart_detail_api_view
+from marketing.views import MarketingPreferenceUpdateView
 from .views import home_page, about_page, contact_page
 
 
@@ -26,6 +27,7 @@ urlpatterns = [
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
+    url(r'settings/email/$', MarketingPreferenceUpdateView.as_view(), name='marketing-pref'),
     url(r'^admin/', admin.site.urls),
 ]
 
--- ecommerce/marketing/views.py
+++ ecommerce/marketing/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.views.generic import UpdateView
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import MarketingPreferenceForm
+from .models import MarketingPreference
+
+
+class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
+    form_class = MarketingPreferenceForm
+    template_name = 'base/forms.html'
+    success_url = '/settings/email/'
+    success_message = 'Your preferences was updated.'
+
+    def dispatch(self, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated():
+            return redirect("/login/?next=/settings/email")
+        return super(MarketingPreferenceUpdateView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(MarketingPreferenceUpdateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Update Email Preferences'
+        return context
+
+    def get_object(self):
+        user = self.request.user
+        obj, created = MarketingPreference.objects.get_or_create(user=user)
+        return obj 

eada3dc524bf6121ac636faaa4da75e0c52b5a8f
Алексей Белов <Alenorze@gmail.com>
2017-12-07 23:59:12 +0200

    Mailchimp Webhook

--- ecommerce/marketing/views.py
+++ ecommerce/marketing/views.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, View
 from django.shortcuts import render, redirect
 
 from .forms import MarketingPreferenceForm
 from .models import MarketingPreference
+from .mixins import CsrfExemptMixin
+from .utils import Mailchimp
+
+MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
 
 
 class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
@@ -28,3 +33,28 @@ class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
         user = self.request.user
         obj, created = MarketingPreference.objects.get_or_create(user=user)
         return obj 
+
+class MailchimpWebhookView(CsrfExemptMixin, View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        list_id = data.get('data[list_id]')
+        if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
+            hook_type = data.get('type')
+            email = data.get('data[email]')
+            response_status, response = Mailchimp().check_subcription_status(email)
+            sub_status = response['status']
+            is_subbed = None
+            mailchimp_subbed = None
+            if sub_status == "subscribed":
+                is_subbed, mailchimp_subbed = (True, True)
+            elif sub_status == "unsubscribed":
+                is_subbed, mailchimp_subbed = (False, False)
+            if is_subbed is not None and mailchimp_subbed is not None:
+                qs = MarketingPreference.objects.filter(user__email__iexact=email)
+                if qs.exists():
+                    qs.update(
+                            subscribed=is_subbed,
+                            mailchimp_subscribed=mailchimp_subbed,
+                            mailchimp_msg=str(data)
+                    )
+            return HttpResponse("Thank you", status=200)

848b11d5eb03117d5d0e6b2a8ec13273169e88b8
Алексей Белов <Alenorze@gmail.com>
2017-12-08 20:00:44 +0200

    Accounts Dropdown

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView, RedirectView
 
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
@@ -15,7 +16,8 @@ from .views import home_page, about_page, contact_page
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
-    url(r'^accounts/', include("accounts.urls", namespace='accounts')),
+    url(r'^accounts/$', RedirectView.as_view(url='/account')),
+    url(r'^account/', include("accounts.urls", namespace='account')),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
@@ -28,6 +30,7 @@ urlpatterns = [
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
+    url(r'^settings/$', RedirectView.as_view(url='/account')),
     url(r'^settings/email/$', MarketingPreferenceUpdateView.as_view(), name='marketing-pref'),
     url(r'^webhooks/mailchimp/$', MailchimpWebhookView.as_view(), name='webhooks-mailchimp'),
     url(r'^admin/', admin.site.urls),

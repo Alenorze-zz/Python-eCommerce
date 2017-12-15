fd1eb49254f484e3763849b3b61871a5c02dff27
Алексей Белов <Alenorze@gmail.com>
2017-12-15 21:33:14 +0200

    File Location Fix

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 
-from ecommetce.aws.utils import ProtectedS3Storage
+from ecommerce.aws.utils import ProtectedS3Storage
 from ecommerce.utils import unique_slug_generator, get_filename
 
 
@@ -103,13 +103,11 @@ pre_save.connect(product_pre_save_receiver, sender=Product)
 
 def upload_product_file_loc(instance, filename):
     slug = instance.product.slug
-    id_ = 0
-    try:
-        id_ = instance.id
-    except:
+    id_ = instance.id
+    if id_ is None:
         Klass = instance.__class__
         qs = Klass.objects.all().order_by('-pk')
-        id_ = qs.first() + 1
+        id_ = qs.first().id + 1
     if not slug:
         slug = unique_slug_generator(instance.product)
     location = "product/{slug}/{id}".format(slug=slug, id=id_)
@@ -119,7 +117,7 @@ class ProductFile(models.Model):
     product       = models.ForeignKey(Product)
     file          = models.FileField(
                         upload_to=upload_product_file_loc,
-                        storage=ProtectedS3Storage(),
+                        storage=ProtectedS3Storage())
     free          = models.BooleanField(default=False)
     user_required = models.BooleanField(default=False)
 

6b575b8bd319ca14b7ae0dcbc8154d6450cc06fd
Алексей Белов <Alenorze@gmail.com>
2017-12-16 00:19:36 +0200

    Aggregate Recent Order

--- ecommerce/analytics/views.py
+++ ecommerce/analytics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Sum, Avg
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
@@ -20,10 +21,10 @@ class SalesView(LoginRequiredMixin, TemplateView):
         qs = Order.objects.all()
         context['orders'] = qs
         context['recent_orders'] = qs.resent().not_refunded()[:5]
-        recent_orders_total = 0 
-        for i in context['recent_orders']:
-            recent_orders_total += i.total
-        context['recent_orders_total'] = recent_orders_total
+        context['recent_orders_total'] = context['recent_orders'].aggregate(
+                                                Sum("total"),
+                                                Avg("total")
+        )
         context['shipped_orders'] = qs.resent().not_refunded().by_status(status='shipped')[:5]
         context['paid_orders'] = qs.resent().not_refunded().by_status(status='paid')[:5]
         return context
